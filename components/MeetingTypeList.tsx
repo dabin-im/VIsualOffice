9c9a7cef815078c40aa7835a94133090e6a5b254
dabin-im <112664372+dabin-im@users.noreply.github.com>
2024-03-31 04:03:06 -0700

    Activate Streaming Camera

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import Image from "next/image";
 import HomeCard from "@/components/HomeCard";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import MeetingModal from "@/components/MeetingModal";
-import {useUser} from "@clerk/nextjs";
-import {Call, useStreamVideoClient} from "@stream-io/video-react-sdk";
-
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useToast } from "@/components/ui/use-toast";
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -18,41 +17,55 @@ const MeetingTypeList = () => {
   const { user } = useUser();
   const client = useStreamVideoClient();
   const [values, setValues] = useState({
-      dateTime: new Date(),
-      description: '',
-      link: ''
+    dateTime: new Date(),
+    description: "",
+    link: "",
   });
+  const [callDetails, setCallDetails] = useState<Call>();
+  const { toast } = useToast();
 
-  const [callDetails, setCallDetails] = useState<Call>()
   const createMeeting = async () => {
-      if (!client || !user) return;
-      try {
-          const id = crypto.randomUUID();
-          const call = client.call('default', id);
-          
-          if(!call) throw new Error('Failed to create call');
-
-          const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
-          const description = values.description || 'Instant meeting';
-
-
-          await call.getOrCreate({
-             data: {
-                 starts_at: startsAt,
-                 custom: {
-                     description
-                 }
-             }
-          });
-
-          setCallDetails(call);
-
-          if (!values.description) {
-              router.push(`/meeting/${call.id}`);
-          }
-      } catch (error) {
-          console.log(error);
+    if (!client || !user) return;
+    try {
+      if (!values.dateTime) {
+        toast({
+          title: "Please select a data and time",
+        });
+        return;
       }
+
+      const id = crypto.randomUUID();
+      const call = client.call("default", id);
+
+      if (!call) throw new Error("Failed to create call");
+
+      const startsAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || "Instant meeting";
+
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          },
+        },
+      });
+
+      setCallDetails(call);
+
+      if (!values.description) {
+        router.push(`/meeting/${call.id}`);
+      }
+      toast({
+        title: "Meeting Created",
+      });
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Failed to create meeting",
+      });
+    }
   };
 
   return (
